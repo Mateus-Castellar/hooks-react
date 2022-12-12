19f31959f5610b8cf2e4999d5f23cd0602f1885d
Mateus Castellar <mateuscastellar@outlook.com>
2022-12-11 22:43:32 -0300

    feat: Setup com react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Hooks</h1>
+      <BrowserRouter>
+        {/* inicio navbar */}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Sobre</Link>
+          </li>
+        </ul>
+        {/* fim navbar */}
+
+        {/* inicio setup rotas */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        {/* fim setup rotas */}
+      </BrowserRouter>
     </div>
   );
 }
