278f60cb2e26a325073285362349ead20afe2046
Mateus Castellar <mateuscastellar@outlook.com>
2022-12-21 09:33:56 -0300

    feat: UseRef com DOM

--- src/components/HookUseRef.js
+++ src/components/HookUseRef.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect, useRef } from "react";
 
 const HookUseRef = () => {
+  //#region useRef
   const numberRef = useRef(0);
   const [counter, setCounter] = useState(0);
   const [counterB, setCounterB] = useState(0);
@@ -12,6 +13,20 @@ const HookUseRef = () => {
     // causa looping infinito (quando altera o valor re-renderiza o componente)
     // setCounter(counter + 1);
   });
+  //#endregion
+
+  //#region useRef com DOM
+  const inputRef = useRef();
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef);
+    setText("");
+    inputRef.current.focus();
+  };
+
+  //#endregion
 
   return (
     <div>
@@ -21,6 +36,16 @@ const HookUseRef = () => {
       <button onClick={() => setCounter(counter + 1)}>contador A</button>
       <p>counter B: {counterB}</p>
       <button onClick={() => setCounterB(counterB + 1)}>contador B</button>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input type="submit" value="enviar" />
+      </form>
       <hr />
     </div>
   );
